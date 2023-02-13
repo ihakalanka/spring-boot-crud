acc14e446802bfea57db13bddfbbc1515f11e47e
ihakalanka <akalankaih19@gmail.com>
2023-02-13 23:27:37 +0530

    added service folder into the test

---
+++ src/test/java/com/akalanka/springbootcrud/service/UserServiceImplTest.java
@@ -0,0 +1,28 @@
+package com.akalanka.springbootcrud.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest {
+
+    @Test
+    void saveUser() {
+    }
+
+    @Test
+    void getUsers() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+
+    @Test
+    void getUserById() {
+    }
+}
